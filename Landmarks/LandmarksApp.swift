cb54346656589f24acb311200427ee47ececdf03
Berkay Barlas <74761090+berkaybarlass@users.noreply.github.com>
2023-11-26 01:51:20 +0300

    First commit try

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by BERKAY BARLAS on 25.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
